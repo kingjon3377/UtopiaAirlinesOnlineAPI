2ac38335ca2d15ce5a49db34427f4f53cdaa767f
Jonathan Lovelace <kingjon3377@gmail.com>
2019-06-21 17:53:04 -0400

    Remove duplicate route.

--- controller/seats_tickets.js
+++ controller/seats_tickets.js
@@ -29,26 +29,6 @@ router.get('/flight/:flightId/seat/:row/:seatId', function(req, res) {
                 res.status(400);
                 res.send('Flight number, row, and seat required');
         } else {
-                request.get(searchEndpoint + '/seat?flight=' +
-                        req.params.flightId + '&row=' + req.params.row +
-                        '&seat=' + req.params.seatId, {}, function(err, response, body) {
-				if (err) {
-					res.status(500);
-					console.log(err);
-					res.send();
-				} else {
-					res.status(response.statusCode);
-					res.send(body);
-				}
-                        });
-        }
-});
-
-router.get('/flight/:flightId/seat/:row/:seatId', function(req, res) {
-        if (!req.params.flightId || !req.params.row || !req.params.seatId) {
-                res.status(400);
-                res.send('Flight number, row, and seat required');
-        } else {
 			// TODO: Switch to booking service for this; search service filters out booked tickets.
                 request.get(searchEndpoint + '/ticket?flight=' +
                         req.params.flightId + '&row=' + req.params.row +

7535cbd003a18eb5cfc2effe5c2ce840a3676f9b
Jonathan Lovelace <kingjon3377@gmail.com>
2019-06-27 12:24:28 -0400

    Remove FIXME comments now that the search service provides the necessary routes.

--- controller/airports.js
+++ controller/airports.js
@@ -4,7 +4,6 @@ const router = require('express').Router();
 const searchEndpoint = process.env.SEARCH_ENDPOINT;
 
 router.get('/airports', function(req, res) {
-	// FIXME: Search service doesn't yet provide this
 	request.get(`${searchEndpoint}/airports`, {}, function(err, response, body) {
 		res.status(response.statusCode);
 		res.send(body);
@@ -12,7 +11,6 @@ router.get('/airports', function(req, res) {
 });
 
 router.get('/airport/:code', function(req, res) {
-	// FIXME: Search service doesn't yet provide this
 	if (!req.params.code) {
 		res.status(400);
 		res.send('Airport code required');
--- controller/flights.js
+++ controller/flights.js
@@ -4,7 +4,6 @@ const router = require('express').Router();
 const searchEndpoint = process.env.SEARCH_ENDPOINT;
 
 router.get('/flights', function(req, res) {
-	// FIXME: Search service doesn't yet provide this
 	request.get(`${searchEndpoint}/flights`, {}, function(err, response, body) {
 		res.status(response.statusCode);
 		res.send(body);
@@ -12,7 +11,6 @@ router.get('/flights', function(req, res) {
 });
 
 router.get('/flight/:flightId', function(req, res) {
-	// FIXME: Search service doesn't yet provide this
 	if (!req.params.flightId) {
 		res.status(400);
 		res.send('Flight number required');
